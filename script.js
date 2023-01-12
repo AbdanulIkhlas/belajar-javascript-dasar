a2655f81a15ffbfe7f81d91f9ebababa75090d6c
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-01-12 22:40:59 +0700

    menambahkan spasi pada line 4 di file script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //JS eksternal
 var x = 10;
 console.log('hello World!');
-console.log('isi dari variabel x adalah' + x)
-console.log('isi dari variabel x adalah' + x);
+console.log('isi dari variabel x adalah' + " " + x);
