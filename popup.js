6b0977f271f1b90db474501ecb1ff0458b937cff
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-01-13 07:55:46 +0700

    mengubah filename script.js menjadi popup.js

---
+++ popup.js
@@ -0,0 +1,36 @@
+// ----- Pop up box 
+/* 
+"alert"   : menampilkan hanya pop up saja 
+"prompt"  : menampilkan pop up dan ada textboxt
+            mengembalikan nilai yang di isi pada textbox,
+"confirm" : memberikan pop up dan memberikan pertanyaan dalam bentuk kondisional,
+            mengembalikan nilai boolean
+*/
+
+// alert("Hello !!");
+// alert("Nama");
+// alert("Saya");
+// alert("Klaz");
+
+// var nama = prompt('masukkan nama : ');
+// alert(nama);
+
+// var tes = confirm('are you sure ?');
+// if (tes == true ) alert('user menekan OK!');
+// else alert('user menekan CANCEL');
+
+alert('Selamat Datang di program mengecek bilangan genap');
+var cek = true;
+
+while(cek){
+    var nilai = prompt('Masukkan nilai : ');
+    if(nilai % 2 == 0){
+        alert('Angka ' + nilai + " adalah genap");
+    }else{
+        alert('Angka ' + nilai + " adalah ganjil");
+    }
+
+    cek = confirm('Coba lagi?');
+}
+
+alert('arigato gozaimasu')
--- script.js
+++
@@ -1,4 +0,0 @@
-//JS eksternal
-var x = 10;
-console.log('hello World!');
-console.log('isi dari variabel x adalah' + " " + x);
