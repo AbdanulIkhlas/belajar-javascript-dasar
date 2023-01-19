947b1f959bbb5e8ac839080d176dc008c1fe6354
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-01-19 13:55:05 +0700

    ADD : Menambahkan function tambah dan kali  di file function.js

--- function.js
+++ function.js
@@ -1,13 +1,24 @@
 // Latihan membuat function
-function menjumlahVolumeKubus(kubus1, kubus2){
-    var jumlah;
-    var volumeKubus1, volumeKubus2;
-    
-    volumeKubus1 = kubus1 * kubus1 * kubus1;
-    volumeKubus2 = kubus2 * kubus2 * kubus2;
-    jumlah = volumeKubus1 + volumeKubus2;
+// function menjumlahVolumeKubus(kubus1, kubus2){
+//     var jumlah;
+//     var volumeKubus1, volumeKubus2;
 
-    return jumlah;
+//     volumeKubus1 = kubus1 * kubus1 * kubus1;
+//     volumeKubus2 = kubus2 * kubus2 * kubus2;
+//     jumlah = volumeKubus1 + volumeKubus2;
+
+//     return jumlah;
+// }
+
+// alert(menjumlahVolumeKubus(8,3));
+
+function tambah(a,b){
+    return a + b;
 }
 
-alert(menjumlahVolumeKubus(8,3));
+function kali(a,b){
+    return a * b;
+}
+
+var hasil = kali(tambah(2,2), tambah(3,3));
+console.log(hasil);
