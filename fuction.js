a71d9e4557394b10849ba4d537cbca26d128e65e
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-01-19 13:26:53 +0700

    CREATE : file function.js

---
+++ fuction.js
@@ -0,0 +1,12 @@
+// Latihan membuat function
+function menjumlahVolumeKubus(kubus1, kubus2){
+    var jumlah;
+    var volumeKubus1, volumeKubus2;
+    volumeKubus1 = kubus1 * kubus1 * kubus1;
+    volumeKubus2 = kubus2 * kubus2 * kubus2;
+    jumlah = volumeKubus1 + volumeKubus2;
+
+    return jumlah;
+}
+
+alert(menjumlahVolumeKubus(8,3));
