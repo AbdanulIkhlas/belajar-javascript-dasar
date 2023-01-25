3fdaad9dca440fd5479fdee699c9ba479fa7a79e
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-01-25 10:45:09 +0700

    ADD : menambahkan code tambahPenumpang

--- programAngkot.js
+++ programAngkot.js
@@ -1,15 +1,13 @@
 // ! PROGRAM PENGELOLAAN PENUMPANG ANGKOT
-var penumpang = [];
+var penumpang = ["klaz", undefined, "gara"];
+
 var tambahPenumpang = function(namaPenumpang, penumpang){
-    console.log(penumpang);
-    console.log(namaPenumpang);
-    
-    if( penumpang.lenght == 0 ){ // jika angkot kosong
+    var isiPenumpang = penumpang.length;
+    if( isiPenumpang == 0 ){ // jika angkot kosong
         penumpang.push(namaPenumpang); // tambah penumpang di awal array
         return penumpang; // kembalikan isi array & keluar dari function
-    }
-    else{ // jika angkot ada penumpang
-        for(var i = 0; i < penumpang.lenght; i++){
+    } else { // jika angkot ada penumpang
+        for(var i = 0; i < isiPenumpang; i++){
             if(penumpang[i] == undefined){ //jika ada kursi kosong
                 penumpang[i] = namaPenumpang;
                 return penumpang;
@@ -18,7 +16,7 @@ var tambahPenumpang = function(namaPenumpang, penumpang){
                 console.log("Penumpang atas nama "+ namaPenumpang + " sudah ada di dalam angkot");
                 return penumpang;
             }
-            if(i == penumpang.lenght - 1){
+            if(i == isiPenumpang - 1){
                 penumpang.push(namaPenumpang);
                 return penumpang;
             }
