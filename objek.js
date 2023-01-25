2b81759f139175bfc8c5e52dfe496579fbe6aca8
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-01-25 21:34:00 +0700

    CREATE : membuat file objek.js

---
+++ objek.js
@@ -0,0 +1,13 @@
+// ! MEMBUAT OBJEK 
+// ? kurang lebih seperti struct
+var mhs = {
+    nama : "Muhammad Abdanul ikhlas",
+    umur : 19,
+    ips : [3.55, 3.88, 3.92],
+    alamat : {
+        jalan : "Jl. TB 4 No. 5",
+        kota : "Yogyakarta",
+        provinsi : "DI Yogyakarta"
+    }
+
+};
